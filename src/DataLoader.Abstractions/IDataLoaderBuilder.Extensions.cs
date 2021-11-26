ba501b156ff123b49a9831957977517310eec881
Jean-Philippe Levesque <jp.levesque@nventive.com>
2021-11-26 09:56:32 -0500

    fix: Fix invalid cast exception when using WithTrigger.

--- src/DataLoader.Abstractions/IDataLoaderBuilder.Extensions.cs
+++ src/DataLoader.Abstractions/IDataLoaderBuilder.Extensions.cs
@@ -67,22 +67,7 @@ namespace Chinook.DataLoader
 			return builder;
 		}
 
-		/// <summary>
-		/// Adds a <see cref="IDataLoaderTrigger"/> in <see cref="IDataLoaderBuilder.TriggerProviders"/>.
-		/// </summary>
-		/// <typeparam name="TData">The type of data.</typeparam>
-		/// <param name="builder">The <see cref="IDataLoaderBuilder"/> reference.</param>
-		/// <param name="triggerProvider">The new <see cref="IDataLoaderTrigger"/> to add.</param>
-		public static IDataLoaderBuilder<TData> WithTrigger<TData>(this IDataLoaderBuilder<TData> builder, Func<IDataLoader<TData>, IDataLoaderTrigger> triggerProvider)
-		{
-			builder.TriggerProviders.Add(GetTriggerProvider);
-			return builder;
-
-			IDataLoaderTrigger GetTriggerProvider(IDataLoader dataLoader)
-			{
-				return triggerProvider((IDataLoader<TData>)dataLoader);
-			}
-		}
+		
 
 		/// <summary>
 		/// Adds a <see cref="IDataLoaderTrigger"/> in <see cref="IDataLoaderBuilder.TriggerProviders"/>.
---
+++ src/DataLoader/Extensions/IDataLoaderBuilder.Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chinook.DataLoader
+{
+	/// <summary>
+	/// Extensions on <see cref="IDataLoaderBuilder"/>.
+	/// </summary>
+	public static class DataLoaderBuilderExtensions2
+	{
+		/// <summary>
+		/// Adds a <see cref="IDataLoaderTrigger"/> in <see cref="IDataLoaderBuilder.TriggerProviders"/>.
+		/// </summary>
+		/// <typeparam name="TData">The type of data.</typeparam>
+		/// <param name="builder">The <see cref="IDataLoaderBuilder"/> reference.</param>
+		/// <param name="triggerProvider">The new <see cref="IDataLoaderTrigger"/> to add.</param>
+		public static IDataLoaderBuilder<TData> WithTrigger<TData>(this IDataLoaderBuilder<TData> builder, Func<IDataLoader<TData>, IDataLoaderTrigger> triggerProvider)
+		{
+			builder.TriggerProviders.Add(GetTriggerProvider);
+			return builder;
+
+			IDataLoaderTrigger GetTriggerProvider(IDataLoader dataLoader)
+			{				
+				return triggerProvider(dataLoader.AsOf<TData>());				
+			}
+		}
+	}
+}
